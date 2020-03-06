59f66d21caa1626ac487d6ff264d845fca096972
Andrew Sturman <ads92@uclive.ac.nz>
2020-03-07 11:36:37 +1300

    Added countries components
    #story[171]

---
+++ client/src/components/Countries.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <p>Countries</p>
+        <div v-for="country in chosenCountries" v-bind:key="country">
+            <Country v-bind:country="country" v-on:deleteCountry="$emit('deleteCountry', country)"></Country>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Country from "./Country";
+    export default {
+        name: "Countries",
+        components:{
+            Country
+        },
+        props: ["chosenCountries"]
+    }
+</script>
+
+<style scoped>
+
+</style>
