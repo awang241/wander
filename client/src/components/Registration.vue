7231e02b8b947f25325a321ad958a708202d29ff
Zara Crisford <zcr11@uclive.ac.nz>
2020-03-10 13:18:15 +1300

    Change design to buefy on registration page
    - Basic elements on registration page all use buefy
    
    #story[171]

--- client/src/components/Registration.vue
+++ client/src/components/Registration.vue
@@ -3,70 +3,78 @@
         <div class="form-container sign-up-container">
             <h1>Create Account</h1>
             <form @submit.prevent="createUser" id="formRegister">
-                <div class="required">
-                    <input v-model="firstName" type="text" placeholder="First Name" name="First Name" id="firstName"
-                           maxlength="25" required>
-                </div>
-                <div>
-                    <input v-model="middleName" type="text" placeholder="Middle Name" name="Last Name" id="middleName"
-                           maxlength="25">
-                </div>
-                <div class="required">
-                    <input v-model="lastName" type="text" placeholder="Last Name" name="Last Name" id="lastName"
-                           maxlength="25" required>
-                </div>
-                <div class="required">
-                    <input v-model="email" type="email" placeholder="Email" name="email" id="email" maxlength="40"
-                           required>
-                </div>
-                <div class="required">
-                    <b-field>
-                             <b-datepicker
-                                     placeholder="Select Date of Birth"
-                                     icon="calendar-today"
-                                     :max-date="maxDate" ref="dateOfBirth"
-                                     v-model="dateOfBirth"
-                                     type="date"
-                                     name="dateOfBirth"
-                                     id="dateOfBirth" expanded required>
-                             </b-datepicker>
-                    </b-field>
-                </div>
 
-                <div class="required">
-                        <b-field>
-                            <b-select size="is-default"
-                                      placeholder="Choose a gender"
-                                      id="gender"
-                                      v-model="gender" expanded required>
-                                <option value="female">Female</option>
-                                <option value="male">Male</option>
-                                <option value="nonBinary">Non Binary</option>
-                            </b-select>
-                        </b-field>
-                </div>
+                <b-field label="First Name">
+                    <b-input v-model="firstName" type="text" placeholder="First Name" maxlength="25" required></b-input>
+                </b-field>
 
-                <div>
-                    <b-field>
-                        <b-select size="is-default" placeholder="Fitness Level" id="fitness" name="fitness" v-model="fitness" expanded>
-                            <option value="0">Beginner: I am not active at all </option>
-                            <option value="1">Novice: I do a low level excercise (walking)</option>
-                            <option value="2">Intermediate: I work out 1-2 times per week </option>
-                            <option value="3">Advanced: I work out 3-4 times per week</option>
-                            <option value="4">Pro: I work out 5+ times per week</option>
-                        </b-select>
-                    </b-field>
-                </div>
+                <b-field label="Middle Name">
+                    <b-input v-model="middleName" type="text" placeholder="Middle Name" maxlength="25"></b-input>
+                </b-field>
 
-                <div class="required">
-                    <input v-model="password" placeholder="Password" type="password" name="password" id="password"
-                           required>
-                </div>
-                <div>
-                    <input type="text" name="nickname" placeholder="Nickname" id="nickname" maxlength="25"
-                           v-model="nickName">
-                    <textarea v-model="bio" placeholder="Bio" id="bio" name="bio" rows="2" cols="30"></textarea>
-                </div>
+                <b-field label="Last Name">
+                    <b-input v-model="lastName" type="text" placeholder="Last Name" maxlength="25" required></b-input>
+                </b-field>
+
+                <b-field label="Email"
+                         type="is-danger"
+                         message="Please enter a valid email">
+                         <b-input type="email"
+                             v-model="email"
+                             placeholder="Email"
+                             name="email"
+                             id="email"
+                             maxlength="30"
+                             icon="email" required>
+                         </b-input>
+                </b-field>
+
+                <b-field label="Date of Birth">
+                         <b-datepicker
+                                 placeholder="Select Date of Birth"
+                                 icon="calendar-today"
+                                 :max-date="maxDate" ref="dateOfBirth"
+                                 v-model="dateOfBirth"
+                                 type="date"
+                                 name="dateOfBirth"
+                                 id="dateOfBirth" expanded required>
+                         </b-datepicker>
+                </b-field>
+
+                <b-field label="Gender">
+                    <b-select size="is-default"
+                              placeholder="Choose a gender"
+                              id="gender"
+                              v-model="gender" expanded required>
+                        <option value="female">Female</option>
+                        <option value="male">Male</option>
+                        <option value="nonBinary">Non Binary</option>
+                    </b-select>
+                </b-field>
+
+                <b-field label="Fitness Level">
+                    <b-select size="is-default" placeholder="Fitness Level" id="fitness" name="fitness" v-model="fitness" expanded>
+                        <option value="0">Beginner: I am not active at all </option>
+                        <option value="1">Novice: I do a low level excercise (walking)</option>
+                        <option value="2">Intermediate: I work out 1-2 times per week </option>
+                        <option value="3">Advanced: I work out 3-4 times per week</option>
+                        <option value="4">Pro: I work out 5+ times per week</option>
+                    </b-select>
+                </b-field>
+
+
+                <b-field label="Password">
+                    <b-input v-model="nickName" type="password" placeholder="Nickname" maxlength="25" password-reveal required></b-input>
+                </b-field>
+
+                <b-field label="Nickname">
+                    <b-input v-model="nickName" type="text" placeholder="Nickname" maxlength="25"></b-input>
+                </b-field>
+
+                <b-field label="Bio">
+                    <b-input v-model="bio" type="textarea" placeholder="Nickname" maxlength="200"></b-input>
+                </b-field>
+
                 <button class="btn btn-light" type="submit">Submit</button>
                 <p>* Indicates a required field</p>
             </form>
@@ -131,33 +139,19 @@
 
 <style scoped>
     .card {
-        min-height: 700px;
+        min-height: 1000px;
     }
 
     h1 {
         padding: 1.5rem 0;
         font-weight: bold;
-        margin: 0;
         text-align: center;
-    }
-
-    h2 {
-        text-align: center;
-    }
-
-    form div {
-        width: 100%;
     }
 
     form {
         background-color: #FFFFFF;
-        display: flex;
         align-items: center;
-        justify-content: center;
-        flex-direction: column;
-        padding: 0 50px;
+        padding: 0 40px;
         text-align: left;
     }
-
-
 </style>
