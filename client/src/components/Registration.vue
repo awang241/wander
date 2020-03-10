bb35c0c6608227997e4112e06997906cc1d5905e
Zara Crisford <zcr11@uclive.ac.nz>
2020-03-10 15:49:36 +1300

    Change design to buefy on registration page
    - Trying to tidy up registration page and use buefy over old css code
    
    #story[171]

--- client/src/components/Registration.vue
+++ client/src/components/Registration.vue
@@ -1,87 +1,97 @@
 <template>
-    <div class="card">
-        <div class="form-container sign-up-container">
-            <h1>Create Account</h1>
-            <form @submit.prevent="createUser" id="formRegister">
+    <section>
+        <div class="container">
+                <h1>Create Account</h1>
+                <form @submit.prevent="createUser" id="formRegister">
 
-                <b-field label="First Name">
-                    <b-input v-model="firstName" type="text" placeholder="First Name" maxlength="25" required></b-input>
-                </b-field>
+                    <b-field grouped>
+                        <b-field label="First Name" expanded>
+                            <b-input v-model="firstName" placeholder="First Name" required></b-input>
+                        </b-field>
+                        <b-field label="Middle Name" expanded>
+                            <b-input v-model="middleName" placeholder="Middle Name"></b-input>
+                         </b-field>
+                        <b-field label="Last Name" expanded>
+                            <b-input v-model="lastName" placeholder="Last Name" required></b-input>
+                        </b-field>
+                    </b-field>
+                    <b-field grouped>
+                        <b-field label="Email" expanded>
+                                 <b-input type="email"
+                                     v-model="email"
+                                     placeholder="Email"
+                                     name="email"
+                                     id="email"
+                                     maxlength="30"
+                                     icon="email" required>
+                                 </b-input>
+                        </b-field>
 
-                <b-field label="Middle Name">
-                    <b-input v-model="middleName" type="text" placeholder="Middle Name" maxlength="25"></b-input>
-                </b-field>
+                        <b-field label="Date of Birth" expanded>
+                                 <b-datepicker
+                                         placeholder="Select Date of Birth"
+                                         icon="calendar-today"
+                                         :max-date="maxDate" ref="dateOfBirth"
+                                         v-model="dateOfBirth"
+                                         type="date" required>
+                                 </b-datepicker>
+                        </b-field>
+                    </b-field>
+                    <b-field grouped>
+                        <b-field label="Gender" expanded>
+                                <b-select
+                                        placeholder="Choose a gender"
+                                        v-model="gender" required>
+                                    <option value="female">Female</option>
+                                    <option value="male">Male</option>
+                                    <option value="nonBinary">Non Binary</option>
+                                </b-select>
+                        </b-field>
 
-                <b-field label="Last Name">
-                    <b-input v-model="lastName" type="text" placeholder="Last Name" maxlength="25" required></b-input>
-                </b-field>
+                        <b-field label="Fitness Level" expanded>
+                            <b-select placeholder="Fitness Level" v-model="fitness">
+                                <option value="0">Beginner: I am not active at all </option>
+                                <option value="1">Novice: I do a low level excercise (walking)</option>
+                                <option value="2">Intermediate: I work out 1-2 times per week </option>
+                                <option value="3">Advanced: I work out 3-4 times per week</option>
+                                <option value="4">Pro: I work out 5+ times per week</option>
+                            </b-select>
+                        </b-field>
+                    </b-field>
+                    <b-field grouped>
+                        <b-field label="Password">
+                            <b-input v-model="password" type="password" placeholder="Password" expanded required></b-input>
+                        </b-field>
 
-                <b-field label="Email"
-                         type="is-danger"
-                         message="Please enter a valid email">
-                         <b-input type="email"
-                             v-model="email"
-                             placeholder="Email"
-                             name="email"
-                             id="email"
-                             maxlength="30"
-                             icon="email" required>
-                         </b-input>
-                </b-field>
+                        <b-field label="Confirm Password">
+                            <b-input v-model="password" type="confpassword" placeholder="Confirm Password" expanded required></b-input>
+                        </b-field>
+                    </b-field>
 
-                <b-field label="Date of Birth">
-                         <b-datepicker
-                                 placeholder="Select Date of Birth"
-                                 icon="calendar-today"
-                                 :max-date="maxDate" ref="dateOfBirth"
-                                 v-model="dateOfBirth"
-                                 type="date"
-                                 name="dateOfBirth"
-                                 id="dateOfBirth" expanded required>
-                         </b-datepicker>
-                </b-field>
+<!--                    <b-field label="Nickname" label-position="on-border">-->
+<!--                        <b-input v-model="nickName" type="text" placeholder="Nickname" maxlength="25"></b-input>-->
+<!--                    </b-field>-->
 
-                <b-field label="Gender">
-                    <b-select size="is-default"
-                              placeholder="Choose a gender"
-                              id="gender"
-                              v-model="gender" expanded required>
-                        <option value="female">Female</option>
-                        <option value="male">Male</option>
-                        <option value="nonBinary">Non Binary</option>
-                    </b-select>
-                </b-field>
+<!--                    <b-field label="Bio" label-position="on-border">-->
+<!--                        <b-input v-model="bio" type="textarea" placeholder="Nickname" maxlength="200"></b-input>-->
+<!--                    </b-field>-->
 
-                <b-field label="Fitness Level">
-                    <b-select size="is-default" placeholder="Fitness Level" id="fitness" name="fitness" v-model="fitness" expanded>
-                        <option value="0">Beginner: I am not active at all </option>
-                        <option value="1">Novice: I do a low level excercise (walking)</option>
-                        <option value="2">Intermediate: I work out 1-2 times per week </option>
-                        <option value="3">Advanced: I work out 3-4 times per week</option>
-                        <option value="4">Pro: I work out 5+ times per week</option>
-                    </b-select>
-                </b-field>
+<!--                    <button class="btn btn-light" type="submit">Submit</button>-->
+<!--                    <p>* Indicates a required field</p>-->
 
-
-                <b-field label="Password">
-                    <b-input v-model="nickName" type="password" placeholder="Nickname" maxlength="25" password-reveal required></b-input>
-                </b-field>
-
-                <b-field label="Nickname">
-                    <b-input v-model="nickName" type="text" placeholder="Nickname" maxlength="25"></b-input>
-                </b-field>
-
-                <b-field label="Bio">
-                    <b-input v-model="bio" type="textarea" placeholder="Nickname" maxlength="200"></b-input>
-                </b-field>
-
-                <button class="btn btn-light" type="submit">Submit</button>
-                <p>* Indicates a required field</p>
-            </form>
+                    <b-field><!-- Label left empty for spacing -->
+                        <b-button  @click="createUser" type="submit">Submit</b-button>
+                    </b-field>
+                </form>
 
         </div>
-    </div>
+    </section>
+
+
+
 </template>
+
+
 
 <script>
     import api from '../Api';
