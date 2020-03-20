3aea20a345067c5fd7d9f2e76427d3df8ff5b357
Andrew Sturman <ads92@uclive.ac.nz>
2020-03-21 10:55:48 +1300

    Added Unit tests for the ListItem component
    -Ensures data being passed into the list item is displayed within the list item
    -Ensures when the user hits the delete button a delete event is emmitted containing the name of the list item
    
    #test #story[171]

---
+++ client/tests/unit/ListItem.spec.js
@@ -0,0 +1,25 @@
+import { shallowMount } from '@vue/test-utils'
+import ListItem from "../../src/components/ListItem";
+
+// Mount the component
+const wrapper = shallowMount(ListItem, {
+    propsData: {
+        listItem: "beans",
+    }
+})
+
+describe('ListItem.vue', () => {
+    it('has a created hook', () => {
+        expect(true).toBe(true)
+    })
+    it('Renders the component', () => {
+        expect(wrapper.exists()).toBe(true)
+    })
+    it('Emits a delete method to its parent containing the name of the list item when the delete button is clicked', () => {
+        wrapper.vm.deleteListItem()
+        expect(wrapper.emitted().deleteListItem[0]).toEqual(["beans"])
+    })
+    it('Displays the correct name of the list item passed into it', () => {
+        expect(wrapper.html()).toContain('beans')
+    })
+});
