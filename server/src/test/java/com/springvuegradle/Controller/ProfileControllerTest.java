445ebd730f1d088f051f42131999295f51fb0996
Zara <zara.crisford4@gmail.com>
2020-04-22 11:58:42 +1200

    Test saving activities
    - Wrote test to check activities are stored in profile correctly when creating a profile and updating a profile.
    
    - Took more time than expected as we had errors with the passport countries being updated in ProfileController.updateProfile function.
    
    #pair[zcr11, kbu34] #story[234]

--- server/src/test/java/com/springvuegradle/Controller/ProfileControllerTest.java
+++ server/src/test/java/com/springvuegradle/Controller/ProfileControllerTest.java
@@ -210,6 +210,25 @@ class ProfileControllerTest {
         assertEquals("randomEmail@gmail.com", emails_from_db_profile.get(0));
     }
 
+    /**
+     * Tests to make sure changing the activities works
+     */
+    @Test
+    void changeActivitiesTest() {
+        Profile originalProfile = createNormalActivitiesProfile();
+        profileController.createProfile(originalProfile);
+        long profileId = repo.findByPrimaryEmail(originalProfile.getPrimary_email()).get(0).getId();
+        Profile expectedProfile = repo.findById(profileId).get();
+
+        Profile updatedProfile = createUpdatedActivitiesProfile();
+        assertEquals(expectedProfile.getActivities().size(), 3, "Check profile saved successfully");
+        profileController.updateProfile(updatedProfile, profileId);
+
+        expectedProfile = repo.findById(profileId).get();
+        assertEquals(expectedProfile.getActivities().size(), 1, "Check activities updated successfully");
+        assertEquals(expectedProfile.getActivities(), updatedProfile.getActivities(), "Check activities updated successfully");
+    }
+
 //    @Test
 //    void testEditProfileNormal(){
 //        Profile testProfile = createNormalProfileJimmy();
@@ -506,6 +525,24 @@ class ProfileControllerTest {
     }
 
     /**
+     * @return a valid profile object with 3 activities.
+     */
+    static Profile createNormalActivitiesProfile() {
+        return new Profile(null, "Jimmy", "Quick", "Jones", "Jim-Jam", "jimjam@hotmail.com", new String[]{"additional@email.com"}, "hushhush",
+                "The quick brown fox jumped over the lazy dog.", new GregorianCalendar(1999, Calendar.NOVEMBER,
+                28), "male", 1, new String[]{}, new String[]{"Football", "Hockey", "Basketball"});
+    }
+
+    /**
+     * @return a valid profile object with updated activities.
+     */
+    static Profile createUpdatedActivitiesProfile() {
+        return new Profile(null, "Jimmy", "Quick", "Jones", "Jim-Jam", "jimjam@hotmail.com", new String[]{"additional@email.com"}, "hushhush",
+                "The quick brown fox jumped over the lazy dog.", new GregorianCalendar(1999, Calendar.NOVEMBER,
+                28), "male", 1, new String[]{}, new String[]{"Hiking"});
+    }
+
+    /**
      * @return a profile object with an invalid country name.
      */
     static Profile createInvalidCountryProfileJimmy() {
@@ -540,5 +577,6 @@ class ProfileControllerTest {
                 "steven@steven.com", new String[]{}, "12345678", "", new GregorianCalendar(1992,
                 Calendar.JUNE, 10), "male", 0, new String[]{}, new String[]{});
     }
+
 }
 
