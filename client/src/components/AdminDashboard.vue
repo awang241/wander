08c16f1b6b0588cb106dbbfadeca25b1443c8c1f
Zara <zara.crisford4@gmail.com>
2020-04-25 11:40:34 +1200

    Implemented new component for the admin dashboard
    - Created new page which will display all user's to the admin.
    - Created a table that will display each user's data
    - Once the endpoint has been created, need to edit variable names to match those and ensure data is being retrieved correctly.
    - Cannot access this page yet - need to put a button in navbar when admin levels are setup so that only admin accounts can access this page.
    
    #story[232]

---
+++ client/src/components/AdminDashboard.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container containerColor">
+        <h1 class="title">Admin Dashboard</h1>
+        <h2 class="title">All Users:</h2>
+
+        <table>
+            <tr>
+                <th>Profile ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Primary Email</th>
+            </tr>
+            <tr v-for="profile in allProfiles" v-bind:key="profile">
+                <td>{{ profile.id }}</td>
+                <td>{{ profile.fname }}</td>
+                <td>{{ profile.lname }}</td>
+                <td>{{ profile.email }}</td>
+            </tr>
+        </table>
+
+    </div>
+</template>
+
+<script>
+    import api from '../Api';
+    export default {
+        name: "AdminDashboard",
+        data() {
+            return {
+                allProfiles: {}
+            }
+        },
+        mounted() {
+            api.getAdminProfiles()
+                .then((response) => {
+                    this.allProfiles = response;
+
+                })
+                .catch(error => console.log(error));
+        }
+    }
+</script>
+
+<style scoped>
+    .containerColor {
+        background-color: #F7F8F9
+    }
+</style>
