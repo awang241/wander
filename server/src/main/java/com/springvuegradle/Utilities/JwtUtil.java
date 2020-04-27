ee18fed2d3009c7ed3fc3c16af5a372b50e73da4
Andrew Sturman <ads92@uclive.ac.nz>
2020-04-27 12:31:06 +1200

    Changed JWT validation to ensure signature of token rather than matching ids. Added extra methods to get claims and added permission claim.
    - Added claim of user role to the JWT and method for getting this
    
    #implement #story[232]

--- server/src/main/java/com/springvuegradle/Utilities/JwtUtil.java
+++ server/src/main/java/com/springvuegradle/Utilities/JwtUtil.java
@@ -1,10 +1,14 @@
 package com.springvuegradle.Utilities;
 
+import com.springvuegradle.Model.Profile;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,8 +21,9 @@ import java.util.function.Function;
 @Service
 public class JwtUtil {
 
+    private final String PERMISSION_KEY = "authLevel";
 
-    private String SECRET_KEY = "keyjklijaijsifjdksnfkdjakfjdkajhlhujdhsgjhfjghkjfhgjkhreuhrehwjkhjfkjhsjgkhfsjkhgkjfsdhgjfjkgshfdkhgfdjgkhs";
+    SecretKey SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
     public Long extractId(String token) {
         return Long.parseLong(extractClaim(token, Claims::getSubject));
@@ -26,6 +31,11 @@ public class JwtUtil {
 
     public Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
+    }
+
+    public Long extractPermission(String token) {
+        String permission = String.valueOf(extractAllClaims(token).get(PERMISSION_KEY));
+        return Long.valueOf(permission);
     }
 
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
@@ -42,36 +52,41 @@ public class JwtUtil {
 
     /**
      * Takes the userdetails object from the user details service and creates a JWT from it
-     * @param id the id associated with the user
+     * @param profile the profile associated with the user
      * @return a JWT generated from the users details
      */
-    public String generateToken(Long id) {
-        //Can pass in claims to be used in the JWT payload in this map
+    public String generateToken(Profile profile) {
         Map<String, Object> claims = new HashMap<>();
-        return createToken(claims, Long.toString(id));
+        claims.put(PERMISSION_KEY, profile.getAuthLevel());
+        return createToken(claims, Long.toString(profile.getId()));
     }
 
     /**
      * Calls JWT API to build a JWT from the person
      * @param claims a list of claims on the token
-     * @param subject the user that is being authenticated
+     * @param userId the user that is being authenticated
      * @return a string showing the JWT
      */
-    private String createToken(Map<String, Object> claims, String subject) {
+    private String createToken(Map<String, Object> claims, String userId) {
 
-        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+        return Jwts.builder().setClaims(claims).setSubject(userId).setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
     }
 
     /**
      * Gets the username and checks whether the username is the same as the username in the user details
-     * @param token
-     * @param profileId The Id of the profile we are checking
+     * @param token the token we are validating
      * @return Whether the token is valid
      */
-    public Boolean validateToken(String token, Long profileId) {
-        final Long tokenId = extractId(token);
-        return (profileId.equals(tokenId) && !isTokenExpired(token));
+    public Boolean validateToken(String token) {
+        try{
+            Jwts.parserBuilder().setSigningKey(SECRET_KEY).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException e){
+            return false;
+        }
+//        final Long tokenId = extractId(token);
+//        return (profileId.equals(tokenId) && !isTokenExpired(token));
     }
 }
