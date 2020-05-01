b1671cae572eb53297a6e9e70ac29f910cb229f8
Phillip Kim <kbu34@uclive.ac.nz>
2020-05-02 00:58:48 +1200

    Added the Activities page to router.js so now its components show up added the main header and a box to accommodate activities #story[233] #implement #fix

--- client/src/components/Activities.vue
+++ client/src/components/Activities.vue
@@ -1,9 +1,19 @@
 <template>
     <div class="container">
-        <h1 class="title">Activities</h1>
-        <div class="list">
+        <div class="container">
+            <h1 class="title is-1">Activities</h1>
+        </div>
+        <b-button type="is-info">
+            Add Activity
+        </b-button>
+        <div class="card">
             <div class="container">
+                <h3 class="title is-5">Activity Name</h3>
+                <div class="content">
+                    <table class="content">
 
+                    </table>
+                </div>
             </div>
         </div>
     </div>
@@ -16,5 +26,8 @@
 </script>
 
 <style scoped>
+    b-button {
+        color: green;
+    }
 
 </style>
--- client/src/router.js
+++ client/src/router.js
@@ -5,6 +5,7 @@ import NavBarComponent from "./components/NavBar.vue";
 import ProfileComponent from "./components/Profile.vue";
 import MainpageComponent from "./components/Mainpage.vue";
 import EditProfileComponent from "./components/editprofile/EditProfile.vue";
+import ActivitiesComponent from "./components/Activities";
 import AdminDashboardComponent from "./components/AdminDashboard";
 import authenticationStore from "./store/authenticationStore";
 
@@ -15,6 +16,7 @@ const routes = [
     {path: "/Registration", name: "registration", component: RegistrationComponent},
     {path: "/NavBar", name: "navbar", component: NavBarComponent},
     {path: "/EditProfile", name: "editProfile", component: EditProfileComponent},
+    {path: "/Activities", name: "activities", component:ActivitiesComponent},
     {
         //This route is only accessible if the user is authenticated, else it sends them back to the main page
         path: "/Profile", name: "profile", component: ProfileComponent, beforeEnter: (to, from, next) => {
