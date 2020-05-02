31b328add3c1325560cd30657aeb89c610194fbf
Zara <zara.crisford4@gmail.com>
2020-05-02 13:41:58 +1200

    Implemented frontend point for creating activities
    #story[233]

--- client/src/Api.js
+++ client/src/Api.js
@@ -13,14 +13,14 @@ const localAxiosInstance = axios.create({
 export default {
   createProfile: (user) => localAxiosInstance.post('/profiles', user),
   login: (user) => localAxiosInstance.post('login', user),
-  editProfile: (userId, newData, sessionId) => localAxiosInstance.put('/profiles/' + userId, newData, {headers: {"authorization":sessionId}}),
-  logout: (userId, sessionId) => localAxiosInstance.post('logout/', userId, {headers: {"authorization":sessionId}}),
-  getProfile: (userId, sessionId) => localAxiosInstance.get('profiles/'+userId, {headers: {"authorization":sessionId}}),
-  editEmail: (emails, userId, sessionId) => localAxiosInstance.put('profiles/'+userId+'/emails', emails, {headers: {"authorization":sessionId}}),
-  editPassword: (passwordDetails, userId, sessionId) => localAxiosInstance.put('profiles/'+ userId+'/password', passwordDetails, {headers: {"authorization":sessionId}}),
+  editProfile: (userId, newData, token) => localAxiosInstance.put('/profiles/' + userId, newData, {headers: {"authorization":token}}),
+  logout: (userId, token) => localAxiosInstance.post('logout/', userId, {headers: {"authorization":token}}),
+  getProfile: (userId, token) => localAxiosInstance.get('profiles/'+userId, {headers: {"authorization":token}}),
+  editEmail: (emails, userId, token) => localAxiosInstance.put('profiles/'+userId+'/emails', emails, {headers: {"authorization":token}}),
+  editPassword: (passwordDetails, userId, token) => localAxiosInstance.put('profiles/'+ userId+'/password', passwordDetails, {headers: {"authorization":token}}),
   getActivityTypesList: () => localAxiosInstance.get('/activityTypes'),
-  getUserProfiles: (sessionId) => localAxiosInstance.get('profiles/', {headers: {"authorization":sessionId}}),
-  getAuthLevel: (sessionId) => localAxiosInstance.get('authLevel/', {headers: {"authorization":sessionId}}),
-
+  getUserProfiles: (token) => localAxiosInstance.get('profiles/', {headers: {"authorization":token}}),
+  getAuthLevel: (token) => localAxiosInstance.get('authLevel/', {headers: {"authorization":token}}),
+  saveActivity: (userId, data, token) => localAxiosInstance.post('/profiles'+userId+'/activities', data, {headers: {"authorization":token}})
 
 }
