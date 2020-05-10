780a5994895c3979b7719c52fc4ab6c73eec3cb1
Phillip Kim <kbu34@uclive.ac.nz>
2020-05-10 16:13:36 +1200

    Removed template activity in activities page #story[233] #fix

--- client/src/components/Activities.vue
+++ client/src/components/Activities.vue
@@ -19,52 +19,6 @@
 
         <hr class ="hrLine">
 
-        <!-- this is a placeholder, please remove after testing -->
-        <div class="container containerColor has-same-height is-gapless">
-            <div class="column">
-                <!-- Activities -->
-                <div class="card">
-                    <div class="card-content">
-                        <h3 class="title is-4">Name</h3>
-                        role:
-                        <div class="content">
-                            <table class="table-profile">
-                                <tr>
-                                    <th colspan="1"></th>
-                                    <th colspan="2"></th>
-                                </tr>
-                                <tr>
-                                    <td>Description:</td>
-                                    <td>description</td>
-                                </tr>
-                                <tr>
-                                    <td>Continous/Duration:</td>
-                                    <td>continuous</td>
-                                </tr>
-                                <tr>
-                                    <td>Start Time:</td>
-                                    <td>time</td>
-                                </tr>
-                                <tr>
-                                    <td>End Time:</td>
-                                    <td>time</td>
-                                </tr>
-                                <tr>
-                                    <td>Location:</td>
-                                    <td>time</td>
-                                </tr>
-                            </table>
-                            <b-button  @click="deleteActivity(1)"
-                                       type="is-danger">
-                                Delete/Leave
-                            </b-button>
-                        </div>
-                        <br>
-                    </div>
-                </div>
-            </div>
-        </div>
-
         <div v-for="activity in activities" v-bind:key="activity">
             <div class="container containerColor has-same-height is-gapless">
                 <div class="column">
