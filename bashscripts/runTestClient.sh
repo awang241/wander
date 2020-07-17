622a031dbec164f9ebd7043a54121622631fd1eb
Cam <cta67@uclive.ac.nz>
2020-07-17 18:44:57 +1200

    Re-adding the fuser commands to the scripts to see if that fixes the backend #story[296]

--- bashscripts/runTestClient.sh
+++ bashscripts/runTestClient.sh
@@ -1,1 +1,2 @@
+fuser -k 9500/tcp || true
 serve -s test-client/dist/ -l 9500
--- bashscripts/runTestServer.sh
+++ bashscripts/runTestServer.sh
@@ -1,1 +1,2 @@
-java -jar test-server/libs/server-4.0.1-SNAPSHOT.jar --server.port=9499
+fuser -k 9499/tcp || true
+java -jar test-server/libs/server-0.0.1-SNAPSHOT.jar --server.port=9499
