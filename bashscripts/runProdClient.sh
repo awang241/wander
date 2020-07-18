932006ba6900bc8473b6727fdbebfddf472f0f98
Cam <cta67@uclive.ac.nz>
2020-07-18 15:01:10 +1200

    add env variables loading to the service scripts. please work #story[296]

--- bashscripts/runProdClient.sh
+++ bashscripts/runProdClient.sh
@@ -1,2 +1,3 @@
 fuser -k 9000/tcp || true
+source /home/gitlab-runner/set-env-vars.txt
 serve -s prod-client/dist/ -l 9000
--- bashscripts/runTestServer.sh
+++ bashscripts/runTestServer.sh
@@ -1,2 +1,3 @@
 fuser -k 9499/tcp || true
+source /home/gitlab-runner/set-env-vars.txt
 java -jar test-server/libs/server-0.0.1-SNAPSHOT.jar --server.port=9499
