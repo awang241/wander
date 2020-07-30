bd61c3d04e2c7e6ca06dfa9b6c344837e2c86c32
sw <swm56@uclive.ac.nz>
2020-07-31 10:29:21 +1200

    - Implemented a repository method for updating roles in the ActivityMembershipRepository
    #story[361], #pair[ads92, swm56]

--- server/src/main/java/com/springvuegradle/repositories/ActivityMembershipRepository.java
+++ server/src/main/java/com/springvuegradle/repositories/ActivityMembershipRepository.java
@@ -2,9 +2,13 @@ package com.springvuegradle.repositories;
 
 import com.springvuegradle.model.ActivityMembership;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface ActivityMembershipRepository extends JpaRepository<ActivityMembership, Long> {
 
+    @Query("UPDATE ActivityMembership  am SET am.role = :role WHERE am.activity = :activityId AND am.profile =: userId")
+    void updateActivityRole(@Param("profileId") Long profileId, @Param("activityId") Long activityId, @Param("role") int role);
 }
