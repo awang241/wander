be6f4b4be4dd5067032e790a17bbdd699741ad9e
Andrew Sturman <ads92@uclive.ac.nz>
2020-09-08 18:15:07 +1200

    Added resizing to map pane and the ability to minimize and restore it - Button on top of the map to minimize and maximize it
    #implement #story[427]

--- client/src/components/MapPane.vue
+++ client/src/components/MapPane.vue
@@ -1,23 +1,51 @@
 <template>
-    <div>
-        <h1>Hello</h1>
-
+    <VueResizable
+            style="border: 2px solid black;"
+            fit-parent="true"
+            :width=width
+            :height=height
+    >
+        <b-button @click="resizePane">{{isMinimized ? "Restore Map" : "Minimize Map"}}</b-button>
         <iframe
-                width="600"
-                height="450"
-                frameborder="0" style="border:0"
+                id="map"
                 src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAWLmvfFRHMuF-D9nmVTAQUYASaUtQoPqQ
             &q=Space+Needle,Seattle+WA" allowfullscreen>
         </iframe>
-    </div>
+    </VueResizable>
 </template>
 
 <script>
+    import VueResizable from 'vue-resizable'
+
     export default {
-        name: "MapPane"
+        name: "MapPane",
+        components: {VueResizable},
+        data() {
+            return {
+                height: 500,
+                width: 500
+            }
+        },
+        computed: {
+            isMinimized() {
+                return this.height === 0 && this.width === 0
+            },
+        },
+        methods: {
+            //Minimizes the map pane if it is not already minimized
+            //Restores the pane to its default size if it is minimized
+            resizePane() {
+                this.height = this.height === 0 ? 500 : 0
+                this.width = this.width === 0 ? 500 : 0
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    #map {
+        width: 100%;
+        height: 100%;
+        position: relative;
+    }
 </style>
