12afdb3d5c89c748095fa7c3048f1d3eca1d1825
Cam <cta67@uclive.ac.nz>
2020-09-08 15:08:16 +1200

    Adding Home Feed to navbar and making it the default page when logging in #story[426]

--- client/src/components/Registration.vue
+++ client/src/components/Registration.vue
@@ -209,7 +209,7 @@
                     localStorage.setItem('userId', response.data.userId)
                     let payload = {'token': response.data.token, 'userId': response.data.userId}
                     store.dispatch('validateByTokenAndUserId', payload).then()
-                    router.push({path: '/Profile/' + store.getters.getUserId})
+                    router.push({path: '/Home'})
                 }))
                     .catch(error => this.warningToast(this.getErrorMessageFromStatusCode(error.response.status)))
             },
