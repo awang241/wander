d0660fe3589615790e4d7c35f5b3a2a23c850e8f
Matthew <mrw118@uclive.ac.nz>
2020-09-12 16:59:33 +1200

    Merged Andrew's map changes into branch - Changed a bit of the EditLocation html #story[428]

--- client/src/components/editprofile/EditLocation.vue
+++ client/src/components/editprofile/EditLocation.vue
@@ -2,32 +2,11 @@
   <div class="container">
     <h1 class="title is-5">Edit Your Location </h1>
 
-        <MapPane/>
+      <MapPane/>
 
-        <br>
-        <br>
-        <br>
+      <br>
 
-        <form>
-
-            <div class="select" id="countrySelectMenu">
-                <select v-model="DropdownCountry" id="country" v-on:change="setAutoCompleteCountry" required expanded>
-                    <option value="" disabled selected> Must select a country</option>
-                    <option v-for="country in possibleCountries" :key="country">
-                        {{country}}
-                    </option>
-                </select>
-            </div>
-            <br>
-            <br>
-
-            <input class="input" type="text" placeholder="Enter a city (required)" id="autocompleteCities"/>
-            <br>
-            <br>
-            <input onkeypress="return /[a-z ]/i.test(event.key)" class="input" type="text" placeholder="Enter a state (optional)" id="autocompleteStates"/>
-
-        </form>
-    <form>
+      <form>
       <input class="input" type="text" placeholder="Enter a location" id="autocompleteLocation"/>
     </form>
 
@@ -82,51 +61,51 @@ let autocompleteLocation;
                     var locationArray = autocompleteLocation.getPlace();
 
 
-        let locationString = "";
-        for (let i = 0; i < (locationArray.address_components).length; i++) {
-          if (i === 0) {
-            locationString = locationArray.address_components[0].long_name;
-          } else if (i !== (locationArray.address_components).length) {
-            if (locationArray.address_components[i].long_name !== locationArray.address_components[i - 1].long_name) {
-                locationString = locationString + ", " + locationArray.address_components[i].long_name;
+                    let locationString = "";
+                    for (let i = 0; i < (locationArray.address_components).length; i++) {
+                      if (i === 0) {
+                        locationString = locationArray.address_components[0].long_name;
+                      } else if (i !== (locationArray.address_components).length) {
+                        if (locationArray.address_components[i].long_name !== locationArray.address_components[i - 1].long_name) {
+                            locationString = locationString + ", " + locationArray.address_components[i].long_name;
+                          }
+                        }
+                      }
+                    document.getElementById("autocompleteLocation").value = locationString;
+                     })
+            },
+
+            clearLocation() {
+              this.$parent.clearLocation()
+              this.successToast("Location removed")
+              document.getElementById("autocompleteLocation").value = null;
+              this.location = {location: ""}
+            },
+            submitLocation() {
+              //Using JSON methods to make a constant and compare two JSON objects
+              const original = JSON.stringify(this.profile.location)
+              this.location = document.getElementById("autocompleteLocation").value;
+              if (this.location === "") {
+                this.warningToast("Please enter a location")
+              } else if (JSON.stringify((this.location)) === original) {
+                this.warningToast("No changes made")
+              } else {
+                this.$parent.updateLocation(this.location)
+                this.successToast("New location saved")
+              }
+            },
+            setLocation() {
+              if (this.profile.location != null) {
+                this.location = this.profile.location;
+                document.getElementById("autocompleteLocation").value = this.location;
               }
             }
+        },
+          async mounted() {
+            this.google = await googleMapsInit();
+            this.setLocation();
+            this.initAutoCompleteLocation();
           }
-        document.getElementById("autocompleteLocation").value = locationString;
-      })
-    },
-
-    clearLocation() {
-      this.$parent.clearLocation()
-      this.successToast("Location removed")
-      document.getElementById("autocompleteLocation").value = null;
-      this.location = {location: ""}
-    },
-    submitLocation() {
-      //Using JSON methods to make a constant and compare two JSON objects
-      const original = JSON.stringify(this.profile.location)
-      this.location = document.getElementById("autocompleteLocation").value;
-      if (this.location === "") {
-        this.warningToast("Please enter a location")
-      } else if (JSON.stringify((this.location)) === original) {
-        this.warningToast("No changes made")
-      } else {
-        this.$parent.updateLocation(this.location)
-        this.successToast("New location saved")
-      }
-    },
-    setLocation() {
-      if (this.profile.location != null) {
-        this.location = this.profile.location;
-        document.getElementById("autocompleteLocation").value = this.location;
-      }
-    }
-  },
-  async mounted() {
-    this.google = await googleMapsInit();
-    this.setLocation();
-    this.initAutoCompleteLocation();
-  }
 
 }
 
