fa10d70df9e1ccb2c2e11d517bba14412fb47475
Andrew Sturman <ads92@uclive.ac.nz>
2020-09-13 18:14:19 +1200

    Managed to get link between entering text into the field and the coordinates picked on a map -Will have to clean up function names and code in general, committing as I don't want to lose working functionality
    #implement #story[428]

--- client/src/components/editprofile/EditLocation.vue
+++ client/src/components/editprofile/EditLocation.vue
@@ -1,36 +1,37 @@
 <template>
-  <div class="container">
-    <h1 class="title is-5">Edit Your Location </h1>
+    <div class="container">
+        <h1 class="title is-5">Edit Your Location </h1>
 
-      <form>
-      <input class="input" type="text" placeholder="Enter a location" id="autocompleteLocation"/>
-    </form>
-    <MapPane marker-label="Profile Location" :location-choice-coordinates="profileLocationLatLong" v-on:locationChoiceChanged="updateLocation"></MapPane>
-    <br>
+        <form>
+            <input class="input" type="text" placeholder="Enter a location" id="autocompleteLocation"/>
+        </form>
+        <MapPane marker-label="Profile Location" :location-choice-coordinates="profileLocationLatLong"
+                 v-on:locationChoiceChanged="updateLocation"></MapPane>
+        <br>
 
-    <div class="row">
-      <br>
-      <b-field style="float:left">
-        <b-button type="is-danger" @click="clearLocation()">Clear fields</b-button>
-      </b-field>
-      <b-field style="float:right">
-        <b-button type="is-primary" @click="submitLocation()">Save</b-button>
-      </b-field>
-      <br>
+        <div class="row">
+            <br>
+            <b-field style="float:left">
+                <b-button type="is-danger" @click="clearLocation()">Clear fields</b-button>
+            </b-field>
+            <b-field style="float:right">
+                <b-button type="is-primary" @click="submitLocation()">Save</b-button>
+            </b-field>
+            <br>
+        </div>
+        <br/>
     </div>
-    <br/>
-  </div>
 </template>
 
 
 <script>
 
-import toastMixin from "../../mixins/toastMixin";
-import googleMapsInit from '../../utils/googlemaps'
-import MapPane from "../MapPane";
+    import toastMixin from "../../mixins/toastMixin";
+    import googleMapsInit from '../../utils/googlemaps'
+    import MapPane from "../MapPane";
 
 
-let autocompleteLocation;
+    let autocompleteLocation;
 
     export default {
         name: "EditLocation",
@@ -47,6 +48,37 @@ let autocompleteLocation;
             }
         },
         methods: {
+            beansTwo(results, status) {
+                if (status === 'OK') {
+                    this.profileLocationLatLong = {
+                        lat: results[0].geometry.location.lat(),
+                        lng: results[0].geometry.location.lng()
+                    }
+                }
+            },
+            beans(autoCompleteLocation) {
+                let locationArray = autoCompleteLocation.getPlace();
+                let locationString = "";
+                for (let i = 0; i < (locationArray.address_components).length; i++) {
+                    if (i === 0) {
+                        locationString = locationArray.address_components[0].long_name;
+                    } else if (i !== (locationArray.address_components).length) {
+                        if (locationArray.address_components[i].long_name !== locationArray.address_components[i - 1].long_name) {
+                            locationString = locationString + ", " + locationArray.address_components[i].long_name;
+                        }
+                    }
+                }
+                this.location = locationString
+                document.getElementById("autocompleteLocation").value = locationString;
+                let geocoder = new this.google.maps.Geocoder;
+                geocoder.geocode({'address': document.getElementById("autocompleteLocation").value}, (results,status) => {
+                          console.log(this)
+                          this.beansTwo(results, status)
+                          console.log("done")
+                        }
+                  )
+                // need to set this variable in local state - isnt working correctly
+            },
             /** This method sets up the autocomplete. It takes the location from the input field and reformats it to a single string.
              This string is saved to the DOM and will be sent to the backend
              There is a lot of logic within the add listener because Google Maps is not in the same scope as Vue. **/
@@ -57,80 +89,59 @@ let autocompleteLocation;
                 // eslint-disable-next-line no-undef
                 autocompleteLocation = new this.google.maps.places.Autocomplete(document.getElementById("autocompleteLocation"), options)
                 autocompleteLocation.setFields(['address_components'])
-                autocompleteLocation.addListener('place_changed', function () {
-                    var locationArray = autocompleteLocation.getPlace();
-                    let locationString = "";
-                    for (let i = 0; i < (locationArray.address_components).length; i++) {
-                      if (i === 0) {
-                        locationString = locationArray.address_components[0].long_name;
-                      } else if (i !== (locationArray.address_components).length) {
-                        if (locationArray.address_components[i].long_name !== locationArray.address_components[i - 1].long_name) {
-                            locationString = locationString + ", " + locationArray.address_components[i].long_name;
-                          }
-                        }
-                      }
-                    document.getElementById("autocompleteLocation").value = locationString;
-                    let geocoder = new this.google.maps.Geocoder;
-                    geocoder.geocode({'address': document.getElementById("autocompleteLocation").value}, function(results, status) {
-                      // need to set this variable in local state - isnt working correctly
-                      if (status === 'OK') {
-                        this.profileLocationLatLong = {lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng()}
-                      }
-                    })
+                autocompleteLocation.addListener('place_changed', this.beans.bind(null, autocompleteLocation))
+            },
+
+            updateLocation(location) {
+                this.profileLocationLatLong = {lat: location.lat(), lng: location.lng()}
+                let geocoder = new this.google.maps.Geocoder;
+                let latlng = {lat: parseFloat(location.lat()), lng: parseFloat(location.lng())};
+                geocoder.geocode({'location': latlng}, function (results, status) {
+                    if (status === 'OK') {
+                        document.getElementById("autocompleteLocation").value = results[0].formatted_address
+                    }
                 })
             },
-
-    updateLocation(location) {
-      this.profileLocationLatLong = {lat: location.lat(), lng: location.lng()}
-      let geocoder = new this.google.maps.Geocoder;
-      let latlng = {lat: parseFloat(location.lat()), lng: parseFloat(location.lng())};
-      geocoder.geocode({'location': latlng}, function(results, status) {
-        if (status === 'OK') {
-          document.getElementById("autocompleteLocation").value = results[0].formatted_address
-        }
-      })
-    },
-
             clearLocation() {
-              this.$parent.clearLocation()
-              this.successToast("Location removed")
-              document.getElementById("autocompleteLocation").value = null;
-              this.location = {location: ""}
+                this.$parent.clearLocation()
+                this.successToast("Location removed")
+                document.getElementById("autocompleteLocation").value = null;
+                this.location = {location: ""}
             },
             submitLocation() {
-              //Using JSON methods to make a constant and compare two JSON objects
-              const original = JSON.stringify(this.profile.location)
-              this.location = document.getElementById("autocompleteLocation").value;
-              if (this.location === "") {
-                this.warningToast("Please enter a location")
-              } else if (JSON.stringify((this.location)) === original) {
-                this.warningToast("No changes made")
-              } else {
-                this.$parent.updateLocation(this.location)
-                this.successToast("New location saved")
-              }
+                //Using JSON methods to make a constant and compare two JSON objects
+                const original = JSON.stringify(this.profile.location)
+                this.location = document.getElementById("autocompleteLocation").value;
+                if (this.location === "") {
+                    this.warningToast("Please enter a location")
+                } else if (JSON.stringify((this.location)) === original) {
+                    this.warningToast("No changes made")
+                } else {
+                    this.$parent.updateLocation(this.location)
+                    this.successToast("New location saved")
+                }
             },
             setLocation() {
-              if (this.profile.location != null) {
-                this.location = this.profile.location;
-                document.getElementById("autocompleteLocation").value = this.location;
-              }
-            }
+                if (this.profile.location != null) {
+                    this.location = this.profile.location;
+                    document.getElementById("autocompleteLocation").value = this.location;
+                }
+            },
         },
-          async mounted() {
+        async mounted() {
             this.google = await googleMapsInit();
             this.setLocation();
             this.initAutoCompleteLocation();
-          }
+        }
 
-}
+    }
 
 </script>
 
 <style scoped>
-.container {
-  background-color: #F7F8F9;
-  margin-top: 0px;
-  padding: 0px;
-}
+    .container {
+        background-color: #F7F8F9;
+        margin-top: 0px;
+        padding: 0px;
+    }
 </style>
