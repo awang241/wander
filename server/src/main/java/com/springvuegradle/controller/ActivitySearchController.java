ba9a8bd51865ebd81019d9207a8fd47d0649d897
Zara <zara.crisford4@gmail.com>
2020-09-23 11:40:05 +1200

    Implemented endpoint method for searching for activities by name. Need to match it up with service method correctly once this has been written. #story[471] #pair[zcr11, swm56]

--- server/src/main/java/com/springvuegradle/controller/ActivitySearchController.java
+++ server/src/main/java/com/springvuegradle/controller/ActivitySearchController.java
@@ -1,17 +1,17 @@
 package com.springvuegradle.controller;
 
+import com.springvuegradle.dto.SimplifiedActivitiesResponse;
 import com.springvuegradle.dto.responses.ActivityLocationResponse;
 import com.springvuegradle.service.ActivitySearchService;
 import com.springvuegradle.service.ActivityService;
 import com.springvuegradle.service.SecurityService;
 import com.springvuegradle.utilities.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -58,4 +58,50 @@ public class ActivitySearchController {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
+
+
+
+
+    /**
+     * Endpoint for searching all activities by name. The request parameters will filter the results to those that match the
+     * search criteria.
+     *
+     * The provided activity name can either be a single word or multiple space-separated words. The provided method can either be
+     * "all" or "any". "All" means all the words in the searched string must match an activity's name. "Any" means any of the words
+     * in the searched string could match an activity's name.
+     * @param token token provided
+     * @param name string pattern to be matched to profile nickname
+     * @param method Whether the user is searching for a user with ALL the required activity types or any of them
+     * @param count number of profiles to be returned.
+     * @param startIndex index
+     * @return response entity containing a list of simplified profiles and an OK status code if the request was successful;
+     * otherwise an empty response with the appropriate error code is returned.
+     */
+    @GetMapping("/activities")
+    public @ResponseBody
+    ResponseEntity<List<SimplifiedActivitiesResponse>> getActivitiesByName(
+            @RequestParam(name = "name", required = true) String name,
+            @RequestParam(name = "method", required = false) String method,
+            @RequestParam(name = "count") int count,
+            @RequestParam(name = "startIndex") int startIndex,
+            @RequestHeader("authorization") String token) {
+        if (!jwtUtil.validateToken(token)) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        } else if (count <= 0) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        int pageIndex = startIndex / count;
+        PageRequest request = PageRequest.of(pageIndex, count);
+        boolean isAdmin = jwtUtil.extractPermission(token) < 2;
+        Long profileId = jwtUtil.extractId(token);
+        try {
+            List<SimplifiedActivitiesResponse> activities = activitySearchService.getActivitiesByName(name, profileId, isAdmin, method, request);
+            return new ResponseEntity<>(activities, HttpStatus.OK);
+        } catch(IllegalArgumentException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+
 }
