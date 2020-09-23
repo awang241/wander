7313b8690f9131711e5615cc691affccac96ae1a
Matthew <mrw118@uclive.ac.nz>
2020-09-23 16:24:48 +1200

    Added jest tests to test the content is correctly formatted by the component #story[430]

--- client/tests/unit/ActivitySearch.spec.js
+++ client/tests/unit/ActivitySearch.spec.js
@@ -40,5 +40,66 @@ describe('ActivitySearch.vue', () => {
         wrapper.vm.chosenActivityTypes = [];
         wrapper.vm.activitySearchType = "all";
         expect(wrapper.vm.getSearchParameters()).toStrictEqual({distance: 50000, latitude: -43.517672, longitude: 172.571489})
+    });
+    test('An activity with a date has their details correctly formatted for the information window', () => {
+        const activity = {
+            activityName: "Fun stuff",
+            activityTypes: ["tennis"],
+            continuous: false,
+            endTime: "2020-09-26T11:00:00+12:00",
+            id: 70001,
+            latitude: 38.8894587,
+            location: "Lincoln Memorial Bookstore, Washington, DC, USA",
+            longitude: -77.0499817,
+            startTime: "2020-09-24T23:00:00+12:00"
+        }
+
+        wrapper.vm.formattedStartTime = activity.startTime.slice(8, 10) + "/" + activity.startTime.slice(5, 7) + "/" + activity.startTime.slice(0, 4) +  " at " + activity.startTime.slice(11, 19);
+        wrapper.vm.formattedEndTime = activity.endTime.slice(8, 10) + "/" + activity.endTime.slice(5, 7) + "/" + activity.endTime.slice(0, 4) +  " at " + activity.endTime.slice(11, 19);
+        wrapper.vm.activityTypesString =
+            `<h1 style="font-size: 16px; font-weight: bold"> Activity Types:</h1>` +
+            `<span style="color:red; font-weight: bold">*</span><span> ${activity.activityTypes[0]} </span>`
+        const expectedFormat =
+            `<div style="width: 100vh; height: 100vh;">` +
+            `<h1 style="font-size: 22px; font-weight: bold; font-style: italic">${activity.activityName}</h1>` +
+            `<h1 style="font-weight: bold">${activity.location}</h1>` +
+            `<br>` +
+            `<h1 style="font-weight: bold">Start date and time: <span>24/09/2020 at 23:00:00</span></h1>` +
+            `<br>` +
+            `<h1 style="font-weight: bold">End date and time: <span>26/09/2020 at 11:00:00</span></h1>` +
+            `<br>` +
+            `<h1 style="font-size: 16px; font-weight: bold"> Activity Types:</h1>` +
+            `<span style="color:red; font-weight: bold">*</span><span> ${activity.activityTypes[0]} </span>`+
+            `</div>`
+        expect(wrapper.vm.formatActivityDetails(activity)).toEqual(expectedFormat)
+    });
+    test('An activity without a date has their details correctly formatted for the information window', () => {
+        const activity = {
+            activityName: "Fun stuff",
+            activityTypes: ["tennis"],
+            continuous: false,
+            endTime: null,
+            id: 70001,
+            latitude: 38.8894587,
+            location: "Lincoln Memorial Bookstore, Washington, DC, USA",
+            longitude: -77.0499817,
+            startTime: null
+        }
+        wrapper.vm.activityTypesString =
+            `<h1 style="font-size: 16px; font-weight: bold"> Activity Types:</h1>` +
+            `<span style="color:red; font-weight: bold">*</span><span> ${activity.activityTypes[0]} </span>`
+        const expectedFormat =
+            `<div style="width: 100vh; height: 100vh;">` +
+            `<h1 style="font-size: 22px; font-weight: bold; font-style: italic">${activity.activityName}</h1>` +
+            `<h1 style="font-weight: bold">${activity.location}</h1>` +
+            `<br>` +
+            `<h1 style="font-weight: bold">Start date: Now</h1>` +
+            `<br>` +
+            `<h1 style="font-weight: bold">End date: Never! "${activity.activityName}" is continuous!</h1>` +
+            `<br>` +
+            `<h1 style="font-size: 16px; font-weight: bold"> Activity Types:</h1>` +
+            `<span style="color:red; font-weight: bold">*</span><span> ${activity.activityTypes[0]} </span>`+
+            `</div>`
+        expect(wrapper.vm.formatActivityDetails(activity)).toEqual(expectedFormat)
     })
 });
