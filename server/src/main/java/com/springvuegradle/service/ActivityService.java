c814cc489d96422eb56aed27320ad290aaea5f58
Phillip Kim <kbu34@uclive.ac.nz>
2020-09-25 12:01:48 +1200

    ensured the user leaving an activity gets a notification #pair[kbu34, mrw118] #fix

--- server/src/main/java/com/springvuegradle/service/ActivityService.java
+++ server/src/main/java/com/springvuegradle/service/ActivityService.java
@@ -220,8 +220,6 @@ public class ActivityService {
             throw new IllegalArgumentException(ActivityMessage.EDITING_CREATOR.toString());
         }
 
-        membershipRepo.deleteActivityMembershipByProfileIdAndActivityId(editedId, activityId);
-
         NotificationType type;
         switch (membership.getRole()) {
             case FOLLOWER:
@@ -248,6 +246,8 @@ public class ActivityService {
                     edited.getFirstAndLastName(), activityName);
         }
         notificationService.createNotification(type, membership.getActivity(), editor, message);
+
+        membershipRepo.deleteActivityMembershipByProfileIdAndActivityId(editedId, activityId);
     }
 
     /**
